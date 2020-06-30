5e4514186fdfd14ead6005be23e40c44029c7a0c
edmundchan <pokedev8@gmail.com>
2020-06-30 15:31:08 -0700

    Adding Login ViewModel

--- Startup.cs
+++ Startup.cs
@@ -46,7 +46,7 @@ namespace ASP_Applications
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Login}/{id?}");
             });
         }
     }
