15f9ddc85742c0433652af460672ea56c96019e2
edmundchan <pokedev8@gmail.com>
2020-07-02 14:36:10 -0700

    Changing startup back to home and index, for smaller projects

--- Startup.cs
+++ Startup.cs
@@ -46,7 +46,7 @@ namespace ASP_Applications
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Account}/{action=Login}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
