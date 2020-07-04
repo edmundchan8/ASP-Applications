4a129cf9558ac92ea8e33b1e7e1caf48d9ad8a40
edmundchan <pokedev8@gmail.com>
2020-07-03 21:22:32 -0700

    Added search functionality

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,9 +11,11 @@ namespace ASP_Applications.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [Display(Name = "Release Date")] //make name appear as this on view
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18,2)")] // Helps EF Core to correctly map Price to currency in database
         public decimal Price { get; set; }
     }
 }
